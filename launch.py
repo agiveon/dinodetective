eda2de5664e5822ad6856858eed177acd8620ecd
Amir Giveon <agiveon@gmail.com>
2021-09-15 10:10:15 -0700

    added 3 classes

--- launch.py
+++ launch.py
@@ -5,7 +5,9 @@ from tensorflow import keras
 
 model = keras.models.load_model('DinoSnapModel.h5')
 
-labels = list(['Ankylosaurus', 'Spinosaurus', 'Stegosaurus', 'T-Rex', 'Triceratops'])
+labels = list(['Ankylosaurus', 'Brachiosaurus', 'Parasaurolophus',
+       'Spinosaurus', 'Stegosaurus', 'T-Rex',
+       'Triceratops', 'Velociraptor'])
 
 IMG_WIDTH = 80
 IMG_HEIGHT = 80
@@ -23,6 +25,6 @@ label = gr.outputs.Label(num_top_classes=3)
 sample_images = [['examples/ex1_ankylosaurus.JPG'],['examples/ex1_spinosaurus.JPG'],['examples/ex1_stegasaurus.JPG']
                  ,['examples/ex1_trex.JPG'],['examples/ex1_triceratops.JPG']]
 title = 'Dino Detective!'
-description = 'Upload a picture of a dinosaur (or choose one from the samples below) and learn which one it is. Currently classifying only (Ankylosaurus, Spinosaurus, Stegosaurus, T-Rex, Triceratops). For best results, place the toy on a flat surface and take a picture of it against a flat background.'
+description = 'Upload a picture of a dinosaur (or choose one from the samples below) and learn which one it is. Currently classifying only 8 types of dinosaurs (Ankylosaurus, Brachiosaurus, Parasaurolophus, Spinosaurus Stegosaurus T-Rex Triceratops Velociraptor). For best results, place the toy on a flat surface and take a picture of it against a flat background.'
 
 gr.Interface(fn=classify_dino, inputs=image, outputs=label, capture_session=True,examples=sample_images, title=title, description=description).launch(share=True)
