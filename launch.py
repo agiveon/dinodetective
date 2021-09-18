d644e43d7e18d41b5ff08f112701a80de92ce371
Amir Giveon <agiveon@gmail.com>
2021-09-18 14:15:39 -0700

    mobilenet version

--- launch.py
+++ launch.py
@@ -5,26 +5,26 @@ from tensorflow import keras
 
 model = keras.models.load_model('DinoSnapModel.h5')
 
-labels = list(['Ankylosaurus', 'Brachiosaurus', 'Parasaurolophus',
-       'Spinosaurus', 'Stegosaurus', 'T-Rex',
-       'Triceratops', 'Velociraptor', 'No Dino Found'])
+labels = list(['Ankylosaurus', 'Brachiosaurus', 'Paceacephalasaurus',
+       'Parasaurolophus', 'Pterodactyl', 'Spinosaurus',
+       'Stegosaurus', 'T-Rex', 'Triceratops',
+       'Velociraptor', 'No Dino Found (please try again)'])
 
-IMG_WIDTH = 80
-IMG_HEIGHT = 80
-
+IMG_SIZE = 224
 
 def classify_dino(inp):
 
-  img_gray = cv2.cvtColor(inp, cv2.COLOR_BGR2GRAY)/255.
-  img_resized = cv2.resize(img_gray,(IMG_WIDTH,IMG_HEIGHT))
-  prediction = model.predict(img_resized.reshape(1,IMG_WIDTH,IMG_HEIGHT,1))
+  img_resize = inp.resize((IMG_SIZE,IMG_SIZE))
+  img_np = np.array(img_resize).astype(np.float32)/255.
+  np.save('img_np_gradio',img_np)
+  prediction = model.predict(img_np.reshape(1,IMG_SIZE,IMG_SIZE,3))
   return {labels[i]: float(prediction[0,i]) for i in range(len(labels))}
 
-image = gr.inputs.Image(shape=(IMG_WIDTH, IMG_HEIGHT))
+image = gr.inputs.Image(type='pil',image_mode="RGB")
 label = gr.outputs.Label(num_top_classes=3)
-sample_images = [['examples/ex1_ankylosaurus.JPG'],['examples/ex1_spinosaurus.JPG'],['examples/ex1_stegasaurus.JPG']
-                 ,['examples/ex1_trex.JPG'],['examples/ex1_triceratops.JPG']]
+sample_images = [['ex1_ankylosaurus.JPG'],['ex1_spinosaurus.JPG'],['ex1_stegasaurus.JPG']
+                 ,['ex1_trex.JPG'],['ex1_triceratops.JPG']]
 title = 'Dino Detective!'
-description = 'Upload a picture of a dinosaur (or choose one from the samples below) and learn which one it is. Currently classifying only 8 types of dinosaurs (Ankylosaurus, Brachiosaurus, Parasaurolophus, Spinosaurus Stegosaurus T-Rex Triceratops Velociraptor). For best results, place the toy on a flat surface and take a picture of it against a flat background. Feedback? Questions? Email me to dinodetect@gmail.com'
+description = 'Upload a picture of a dinosaur (or choose one from the samples below) and learn which one it is. \n Currently classifying only (Ankylosaurus, Brachiosaurus, Paceacephalasaurus, Parasaurolophus, Pterodactyl, Spinosaurus, Stegosaurus, T-Rex, Triceratops, Velociraptor)'
 
-gr.Interface(fn=classify_dino, inputs=image, outputs=label, capture_session=True,examples=sample_images, title=title, description=description).launch(share=True)
+gr.Interface(fn=classify_dino, inputs=image, outputs=label, capture_session=True,examples=sample_images, title=title, description=description).launch(debug=True)
